8ccca3e0fa75806d2b7289c9226e927082ee81fa
Katrina Dierking <kalynndier@gmail.com>
2020-09-12 12:46:17 -0400

    ex 2 passed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,37 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
+    #set empty dictionary and list
+    flight = {}
+    route = []
+    
     """
-    YOUR CODE HERE
+    * loop through the param length
+    * key set to ticket's starting point
+    * value set to ticket's next airport along the way
+    * set flight to current stop with key/value
     """
-    # Your code here
+    for i in range(length): 
+        key = tickets[i].source
+        value = tickets[i].destination
+        flight[key] = value    
+    
+    """
+    * key "None" = start of flight
+    * key/value "None" = end of flight
+    * add stops to the route until key/value is "None"
+    """
+    
+    #start of flight
+    key = flight["NONE"]
+    
+    #add stops to the route
+    while key !="NONE":
+        route.append(key)
+        #set next stop in flight
+        key = flight[key]
+        
+    #Add final stop so key/value is "None"
+    route.append(key)
 
     return route
